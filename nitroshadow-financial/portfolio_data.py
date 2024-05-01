265ae28dbc9fe77d7b2699a9bdf48604a161bfc7
nitroshadow <shadowdragonqc@gmail.com>
2024-05-01 15:52:13 -0400

    Add tableExists method

--- nitroshadow-financial/portfolio_data.py
+++ nitroshadow-financial/portfolio_data.py
@@ -7,3 +7,11 @@ class PortfolioData:
         print('PortfolioData')
         self.connection = sqlite3.connect('./data/' + self.DATABASE_NAME)
         self.cursor = self.connection.cursor()
+
+    def tableExists(self, name):
+        query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{name}'"
+        result = self.cursor.execute(query)
+
+        if result.fetchone() is None:
+            return False
+        return True
